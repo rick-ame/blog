336e03c28129005c3869c71488e51e5d129af898
Rick <rick.ast@outlook.com>
2024-03-25 07:38:51 +0000

    add playwright for e2e testing

--- app/blog/[...slug]/_components/callout.tsx
+++ app/blog/[...slug]/_components/callout.tsx
@@ -13,7 +13,7 @@ export const Callout: FC<PropsWithChildren<Props>> = ({
   return (
     <div
       className={cn(
-        'my-6 w-full items-start rounded-md border border-l-4 p-4 dark:max-w-none',
+        'my-6 w-full items-start rounded-md border border-l-4 px-4 dark:max-w-none',
         {
           'border-red-900 bg-red-50 dark:prose': type === 'danger',
           'border-yellow-900 bg-yellow-50 dark:prose': type === 'warning',
