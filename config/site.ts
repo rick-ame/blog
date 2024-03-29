4a937ef8ec1853be14cfe41f27cf52e2192934c4
Rick <rick-ast@outlook.com>
2024-03-29 14:14:42 +0000

    update my email address

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: 'Writer',
   description: 'My Personal blog about writing',
   author: 'Rick',
-  email: 'rick.ast@outlook.com',
+  email: 'rick-ast@outlook.com',
   avatar: 'https://avatars.githubusercontent.com/u/130942798?v=4',
   links: {
     github: 'https://github.com/rick-ast',
