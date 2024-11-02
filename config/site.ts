0829cb825cb2f4ec41989833d1e31a5be9bef6d5
Rick <rick-ame@outlook.com>
2024-11-03 02:11:14 +0800

    update contact info

--- config/site.ts
+++ config/site.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
-  name: 'AST',
+  name: 'Ame',
   description: 'My Personal Blog',
   author: 'Rick',
-  email: 'rick-ast@outlook.com',
+  email: 'rick-ame@outlook.com',
   avatar: 'https://avatars.githubusercontent.com/u/130942798?v=4',
   links: {
-    github: 'https://github.com/rick-ast',
+    github: 'https://github.com/rick-ame',
   },
 }
 
