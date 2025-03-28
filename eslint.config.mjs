8500a10a7dfc9abcd81272f8cc756be677b3e968
Rick <rick-ame@outlook.com>
2025-03-28 11:48:28 +0800

    fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
 
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
