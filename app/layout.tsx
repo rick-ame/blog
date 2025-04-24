d3be5e6b5417696a44eb843c3229ea49f3928c75
Rick <rick-ame@outlook.com>
2025-04-24 16:55:42 +0800

    chore: cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,11 @@
 import './globals.css'
 
 import type { Metadata, Viewport } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
 import { Providers } from '@/components/providers'
 import { siteConfig } from '@/config/site'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -37,12 +26,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="scroll-pt-[3.5rem]">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" suppressHydrationWarning>
+      <body className="scroll-pt-14 antialiased">
         <Providers>
-          <div className="relative flex min-h-dvh flex-col">
+          <div className="flex min-h-dvh flex-col">
             <Header />
             <main className="flex-1">{children}</main>
             <Footer />
